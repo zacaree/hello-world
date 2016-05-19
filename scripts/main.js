7a74f95ac8bf0b1d99f9f281e7d1bb87eec76fe1
zacaree <zacharem@gmail.com>
2016-05-19 16:12:52 -0500

    Using jQuery to interact with panels on my page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,50 @@
 
+// Panels
+$(document).ready(function() {
+
+  $('.btn-panel').on('click', function() {
+    var panelId = $(this).attr('data-panelid');
+    $('#'+panelId).fadeToggle();
+  });
+
+});
+
+
+
+
+// Checklist
+var checklist = document.getElementById("checklist")
+
+var items = checklist.querySelectorAll("li");
+var inputs = checklist.querySelectorAll("input");
+
+for (var i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", editItem);
+  inputs[i].addEventListener("blur", updateItem);
+  inputs[i].addEventListener("keypress", itemKeypress);
+}
+
+function editItem() {
+  this.className = "edit";
+  var input = this.querySelector("input");
+  input.focus();
+  input.setSelectionRange(0, input.value.length);
+}
+
+function updateItem() {
+  this.previousElementSibling.innerHTML = this.value;
+  this.parentNode.className = "";
+}
+
+function itemKeypress(event) {
+  if (event.which === 13) {
+    updateItem.call(this);
+  }
+}
+
+
+
+
 // Calculator
 var numOne = document.getElementById("num-one");
 var numTwo = document.getElementById("num-two");
@@ -35,36 +81,3 @@ function picLink() {
     pic.className = "hide";
   }
 };
-
-
-
-
-// Checklist
-var checklist = document.getElementById("checklist")
-
-var items = checklist.querySelectorAll("li");
-var inputs = checklist.querySelectorAll("input");
-
-for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener("click", editItem);
-  inputs[i].addEventListener("blur", updateItem);
-  inputs[i].addEventListener("keypress", itemKeypress);
-}
-
-function editItem() {
-  this.className = "edit";
-  var input = this.querySelector("input");
-  input.focus();
-  input.setSelectionRange(0, input.value.length);
-}
-
-function updateItem() {
-  this.previousElementSibling.innerHTML = this.value;
-  this.parentNode.className = "";
-}
-
-function itemKeypress(event) {
-  if (event.which === 13) {
-    updateItem.call(this);
-  }
-}
