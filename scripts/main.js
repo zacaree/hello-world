50add77691a3bcc222e4c59c542bb7a9e07d91c3
zacaree <zacharem@gmail.com>
2016-05-25 13:16:59 -0500

    Created a basic jQuery image slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,48 @@
 
-
-
 // Basic jQuery slider
+$(document).ready(function() {
+
+  // Configuration
+  var width = 1140;
+  var animationSpeed = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+
+  // Cache DOM
+  var $slider = $('#slider'); // This only searches the HTML once, right here.
+  var $slideContainer = $slider.find('.slides'); // This and the next var, searches the previous var instead of the whole document making things faster
+  var $slides = $slideContainer.find('.slide');
+
+  var interval;
+
+  function startSlider(){
+    interval = setInterval(function() {
+      $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+        currentSlide++;
+        if (currentSlide === $slides.length) {
+          currentSlide = 1;
+          $slideContainer.css('margin-left', 0);
+        };
+      });
+    }, pause);
+  };
+
+  function stopSlider() {
+    clearInterval(interval);
+  };
+
+  $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+  startSlider();
+
+})
 
 
 
+
+// var myInterval = setInterval(function() {
+//   console.log(new Date());
+// }, 1000);
 
 
 
